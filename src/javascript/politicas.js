e82a140e5702808d4b309a017b960e68cf06ab4e
coderblue10 <produccionesbog@gmail.com>
2023-12-20 10:40:21 -0500

    culminación del proyecto, Continental de servicios, pagina web estructurada para una empresa de mudanzas

--- src/javascript/politicas.js
+++ src/javascript/politicas.js
@@ -1,3 +1,7 @@
+const menuMobile = document.querySelector('.mobile-menu')
+
+
+
 const home = document.querySelector('.home').addEventListener('click', ()=>{
     window.location.href = '../index.html'
 })
@@ -12,5 +16,27 @@ const testimonials = document.querySelector('.testimonials').addEventListener('c
     window.location.href = '../index.html#section-six'
 })
 const form = document.querySelector('.contactHeader').addEventListener('click',()=>{
+    window.location.href = '../php/index.php'
+})
+const menuMobileButton = document.getElementById('icon-menu').addEventListener('click', ()=>{
+    menuMobile.style.display = 'flex'
+})
+const buttonColse = document.querySelector('.menu-mobile-icon').addEventListener('click',()=>{
+    menuMobile.style.display = 'none'
+})
+const homeMobile = document.querySelector('.homeMobile').addEventListener('click',()=>{
+    window.location.href = '../index.html'
+})
+const aboutMobile = document.querySelector('.aboutUsMobile').addEventListener('click',()=>{
+    window.location.href = '../html/aboutme.html'
+})
+const testimonialsMobile = document.querySelector('.testimonialsMobile').addEventListener('click',()=>{
+    window.location.href = '../index.html#section-six'
+})
+const portafolioMobile = document.querySelector('.portfolioMobile').addEventListener('click',()=>{
+    window.open('https://portafolio.continentaldetrasteos.com/')
+    window.close()
+})
+const contact = document.querySelector('.contactUsMobile').addEventListener('click',()=>{
     window.location.href = '../php/index.php'
 })
